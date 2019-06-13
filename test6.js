295d69efca19c0190c1901c98e5234ab848d7bae
takase_2617 <h.takase@srmtlab.org>
2019-06-13 15:59:01 +0900

    console cards item

--- test6.js
+++ test6.js
@@ -35,7 +35,7 @@ var orig_div = $("div.swiper-slide");
 console.log(orig_div);
 var parent = orig_div.parent();
 console.log(parent);
-for (item in cards){ 
+for(var item in cards){ 
 	console.log(item);
 	var url = item["url"];
 	console.log(url);
