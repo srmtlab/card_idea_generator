0bdba07f61ec79f080237e2a363a75bb1b2efaef
YotaShimizu <sytytuyao@yahoo.co.jp>
2019-06-17 20:21:54 +0900

    変更点はswiper_test.htmlを参照

--- test6.js
+++ test6.js
@@ -1,7 +1,5 @@
-var cards = [];
+let cards = [];
 
-console.log("aaaaaa");
-// alert("aaaaaaaa");
 $(function(){
 	
 	$.ajax({
@@ -11,41 +9,60 @@ $(function(){
 	})
 	.then(
 		function(json){
-			for(var i = 0; i < json.length; i++){
+			for(let i = 0; i < json.length; i++) {
 				cards.push({
 					"url": json[i].url,
 					"title": json[i].title
 				});
-			};
-		
-	for(var i = 0; i < cards.length; i++){
-		alert("Card's url is " + cards[i]["url"] + " and the title is a " + cards[i]["title"] + ".");
-	};
+			}
+
+			$(".img").attr("src", cards[0].url);
+			let copy = $(".swiper-slide");
+			for(let i = 0; i < cards.length - 1; i++){
+				copy.clone().appendTo(".swiper-wrapper");
+				$(".img:eq(" + (i + 1) + ")").attr("src", cards[i + 1].url);
+			}
+
 		},
 		function(){
 			alert("error:ファイルの読み込みに失敗");
 		}
 	);
-	
+
+	//「送信」ボタンクリック時のイベント
+	let btn_i = 0;//テスト用変数
+	$(".btn").on("click", function(){
+
+		//テスト用イベント#################
+		alert(cards[btn_i++].url);
+		if(btn_i > cards.length - 1){
+			btn_i = 0;
+		}
+		//#################################
+
+	})
+
+	//画像内の矢印を表示・非表示
+	let s_c = $(".swiper-container");
+	let s_b_p = $(".swiper-button-prev");
+	let s_b_n = $(".swiper-button-next");
+	s_c.on("mouseenter", function(){
+		s_b_p.stop().animate({
+			opacity:"1"
+		},{duration:200});
+		s_b_n.stop().animate({
+			opacity:"1"
+		},{duration:200});
+	});
+	s_c.on("mouseleave", function(){
+		s_b_p.stop().animate({
+			opacity:"0"
+		},{duration:200});
+		s_b_n.stop().animate({
+			opacity:"0"
+		},{duration:200});
+	});
+
 
 	
 });
-
-console.log(cards[0]);
-
-var orig_div = $("div.swiper-slide");
-console.log(orig_div);
-var parent = orig_div.parent();
-console.log(parent);
-for(var item in cards){ 
-	console.log(item);
-	var url = item["url"];
-	console.log(url);
-	var div =orig_div.clone();
-	console.log(div);
-	div.appendTo(parent);
-	var img = div.children("img");
-	console.log(img);
-	img.attr("src", url);
-	console.log(img);
-};
